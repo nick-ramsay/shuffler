369415ef8e8916dd628131674e30ba7e6f60c7be
nick-ramsay <NickRamsay1989@gmail.com>
2020-11-12 19:52:17 +1100

    Items now added and appended. Still needs shuffle logic

--- assets/js/shuffle.js
+++ assets/js/shuffle.js
@@ -1,12 +1,35 @@
-const addItem = () => {
-    console.log("I clicked a button!");
-    let addItemInputValue = addItemInput.value;
-    console.log(addItemInputValue);
+const shuffleBtn = document.getElementById("shuffle-btn");
+const clearBtn = document.getElementById("clear-btn");
+const addItemInput = document.getElementById("shuffle-item");
+const shuffledItemDiv = document.getElementById("shuffled-items");
+let items = [];
 
+const addItem = () => {
+    addItemInput.focus();
+    let addItemInputValue = addItemInput.value;
+
+    if (addItemInputValue !== "") {
+        document.getElementById("shuffled-items").innerHTML = "";
+        items.push(addItemInputValue);
+
+        for (let i = 0; i < items.length; i++) {
+            let newItemEl = document.createElement('p');
+            newItemEl.id = "item" + i;
+            newItemEl.innerHTML = (i + 1) + ". " + items[i];
+
+            document.getElementById("shuffled-items").appendChild(newItemEl);
+        }
+
+    }
     addItemInput.value = "";
 }
 
-const shuffleBtn = document.getElementById("shuffle-btn");
-const addItemInput = document.getElementById("shuffle-item");
+const clearItems = () => {
+    addItemInput.focus();
+    document.getElementById("shuffled-items").innerHTML = "";
+    items = [];
+    addItem();
+}
 
-shuffleBtn.addEventListener("click",addItem);
+clearBtn.addEventListener("click", clearItems);
+shuffleBtn.addEventListener("click", addItem);
