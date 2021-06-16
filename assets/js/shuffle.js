72e8c27e6818b48dc48ce530472a60ad711a5c52
Nick Ramsay <NickRamsay1989@gmail.com>
2021-06-16 21:29:04 +1000

    Committing most recent changes for shuffle and local storage

--- assets/js/shuffle.js
+++ assets/js/shuffle.js
@@ -3,27 +3,48 @@ const shuffleBtn = document.getElementById("shuffle-btn");
 const clearBtn = document.getElementById("clear-btn");
 const addItemInput = document.getElementById("shuffle-item");
 const shuffledItemDiv = document.getElementById("shuffled-items");
+
 let items = [];
-let usedIndexes = [];
+
+const populateList = () => {
+
+    if (localStorage.items === undefined) {
+        localStorage.items = ["null"];
+    }
+
+    let currentItemList = localStorage.items.split(',');
+    console.log(localStorage.items);
+    document.getElementById("shuffled-items").innerHTML = "";
+
+    if (currentItemList[0] !== "null") {
+        for (let i = 0; i < currentItemList.length; i++) {
+            let newItemEl = document.createElement('p');
+            newItemEl.id = "item" + i;
+            newItemEl.innerHTML = (i + 1) + ". " + currentItemList[i];
+
+            document.getElementById("shuffled-items").appendChild(newItemEl);
+        }
+    }
+}
 
 const addItem = () => {
     addItemInput.focus();
+    
+    let currentItems = localStorage.items.split(',');
+
+    currentItems[0] === "null" && currentItems.length > 1 ? currentItems.splice(0):"";
+
     let addItemInputValue = addItemInput.value;
 
     if (addItemInputValue !== "") {
         document.getElementById("shuffled-items").innerHTML = "";
-        items.push(addItemInputValue);
-
-        for (let i = 0; i < items.length; i++) {
-            usedIndexes.push(i);
-            let newItemEl = document.createElement('p');
-            newItemEl.id = "item" + i;
-            newItemEl.innerHTML = (i + 1) + ". " + items[i];
-
-            document.getElementById("shuffled-items").appendChild(newItemEl);
-        }
-
+        currentItems.push(addItemInputValue);
     }
+
+    localStorage.items = currentItems;
+
+    populateList();
+
     addItemInput.value = "";
 }
 
@@ -31,14 +52,34 @@ const clearItems = () => {
     addItemInput.focus();
     document.getElementById("shuffled-items").innerHTML = "";
     items = [];
-    addItem();
+
+    localStorage.setItem("items", "null");
+
+    populateList();
 }
 
 const shuffleItems = () => {
-    console.log("Shuffle Items clicked...");
-    console.log(items.length);
+    let orderedItems = [];
+    let usedIndexes = [];
+    let currentIndex;
+
+    for (let i = 0; usedIndexes.length < items.length; i++) {
+        currentIndex = Math.floor((Math.random() * items.length));
+
+        if (usedIndexes.indexOf(currentIndex) === -1) {
+            usedIndexes.push(currentIndex);
+            orderedItems.push(items[currentIndex]);
+        }
+
+    }
+
+    items = orderedItems;
+
+    localStorage.setItem("items", items);
+
+    populateList();
 }
 
 clearBtn.addEventListener("click", clearItems);
 addBtn.addEventListener("click", addItem);
-shuffleBtn.addEventListener("click",shuffleItems);
+shuffleBtn.addEventListener("click", shuffleItems);
